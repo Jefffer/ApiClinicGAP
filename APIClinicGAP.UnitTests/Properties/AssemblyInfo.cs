af2aa501979c53cd9b3b1dee98dd077ff2e2f8a3
Jefffer <jefre123@hotmail.com>
2019-10-18 22:48:12 -0500

    UnitTests added. Stable version

---
+++ APIClinicGAP.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("APIClinicGAP.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("APIClinicGAP.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("59f9104e-649c-4f78-837b-e00bdd029602")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ APIClinicGAP.UnitTests/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApiClinicGAP.Controllers;
+using WebApiClinicGAP.Models;
+
+namespace APIClinicGAP.UnitTests
+{
+    [TestClass]
+    public class AppointmentTests
+    {
+        [TestMethod]
+        public void PostAppointment_CorrectAppointment_ReturnsTrue()
+        {
+            // Arrange
+            var appointmentController = new AppointmentsController();
+
+            // Act
+            //var result = appointmentController.PostAppointments(new Appointments
+            //{
+            //    fk_idPatient = 1,
+            //    fk_idAppointmentType = 1,
+            //    fk_idDoctor = 1,
+            //    isActive = true,
+            //    AppointmentDateTime = DateTime.Now
+            //});
+
+        }
+    }
+}
